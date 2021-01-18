a5d9a19ebdd146ad4bebac94bd4f1caaa317c358
Akila Mendis <akila@oneit.com.au>
2021-01-18 09:40:19 +0530

    198 Navigator Hookup

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import { createAppContainer } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import SignupScreen from './src/screens/SignUpScreen';
 import AccountScreen from './src/screens/AccountScreen';
 import SigninScreen from './src/screens/SigninScreen';
@@ -7,17 +8,21 @@ import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
 
-const navigator = createStackNavigator(
-  {
-    Signup: SignupScreen,
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator(
+    {
+      Signup: SignupScreen,
+      Signin: SigninScreen
+  
+    }),
+    mainFlow: createBottomTabNavigator({
+      trackListFlow: createStackNavigator({
+        TrackList: TrackListScreen,
+        TrackDetail: TrackDetailScreen
+      }),
+      TrackCreate: TrackCreateScreen,
+      Account: AccountScreen
+    })
+});
 
-  },
-  {
-    initialRouteName: "Signup",
-    defaultNavigationOptions: {
-      title: "Tracks"
-    }
-  }
-);
-
-export default createAppContainer(navigator);
+export default createAppContainer(switchNavigator);
