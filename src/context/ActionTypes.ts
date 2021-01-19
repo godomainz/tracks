2dbfc886b8b5d2339d5616de3dc8e84763d98bb9
Akila Mendis <akila@oneit.com.au>
2021-01-19 14:13:33 +0530

    added signin screen

--- src/context/ActionTypes.ts
+++ src/context/ActionTypes.ts
@@ -3,13 +3,15 @@ export const SIGN_UP = "SIGN_UP";
 export const SIGN_IN = "SIGN_IN";
 export const SIGN_OUT = "SIGN_OUT";
 export const ADD_ERROR = "ADD_ERROR";
+export const SIGNIN_ERROR = "SIGNIN_ERROR";
 
 export interface Action {
-    type: typeof SIGN_UP | typeof SIGN_IN | typeof SIGN_OUT | typeof ADD_ERROR,
+    type: typeof SIGN_UP | typeof SIGN_IN | typeof SIGN_OUT | typeof ADD_ERROR | typeof SIGNIN_ERROR,
     payload: Auth | any
 }
 
 export type SignUpAction = { type: typeof SIGN_UP, payload: string }
-export type SignInAction = { type: typeof SIGN_IN, payload: Auth }
+export type SignInAction = { type: typeof SIGN_IN, payload: string }
 export type SignOutAction = { type: typeof SIGN_OUT }
 export type AddErrorAction = { type: typeof ADD_ERROR, payload: string[] }
+export type SignInErrorAction = { type: typeof SIGNIN_ERROR, payload: string[] }
--- src/context/AuthContext.ts
+++ src/context/AuthContext.ts
@@ -23,6 +23,10 @@ const authReducer = (state:Auth, action:actionTypes.Action): Auth => {
             return {...state, errorMessage: action.payload}
         case actionTypes.SIGN_UP:
             return {errorMessage: null, token: action.payload}
+        case actionTypes.SIGNIN_ERROR:
+            return {...state, errorMessage: action.payload}
+        case actionTypes.SIGN_IN:
+            return {errorMessage: null, token: action.payload}
         default:
             return state;
     }
@@ -36,7 +40,7 @@ const storeData = async (key :string,value:string) => {
     }
   }
 
-  const getData = async (key:string) => {
+const getData = async (key:string) => {
     try {
       const value = await AsyncStorage.getItem(key);
       if(value !== null) {
@@ -47,6 +51,18 @@ const storeData = async (key :string,value:string) => {
     }
   }
 
+const getErrors = (response: Object):string[] => {
+    const errors:Object = response;
+    let errorList: string[] = [];
+    for(const [key, value] of Object.entries(errors)){
+        value.map((el:any)=>{
+            errorList.push(el);
+        });
+    }
+    console.log(errorList)
+    return errorList;
+}
+
 const signup = (dispatch:(action: actionTypes.SignUpAction | actionTypes.AddErrorAction)=> void) => async ( {email, password}:Login ) => {
         try {
             const response = await trackerApi.post('signup/', { email, password});
@@ -54,25 +70,25 @@ const signup = (dispatch:(action: actionTypes.SignUpAction | actionTypes.AddErro
             dispatch({ type: actionTypes.SIGN_UP, payload: response.data.token });
             navigate('TrackList');
         } catch (error) {
-            const errors:Object = error.response.data;
-            let errorList: string[] = [];
-            for(const [key, value] of Object.entries(errors)){
-                value.map((el:any)=>{
-                    errorList.push(el);
-                });
-            } 
-            dispatch({type: actionTypes.ADD_ERROR, payload: errorList });
+            dispatch({type: actionTypes.ADD_ERROR, payload: getErrors(error.response.data) });
         }
     };
 
 
-const signin = (dispatch:()=>actionTypes.SignUpAction) => {
-    return ( {email, password}:Login ) => {
-        // Try to signin
-        // Handle success by updating state
-        // Handle failure by showing error message (somehow)
-    }
-}
+const signin = (dispatch:(action: actionTypes.SignInAction | actionTypes.SignInErrorAction)=> void) => async ( {email, password}:Login ) => {
+        
+        try {
+            console.log("SIGNIN");
+            console.log(`${email}, ${password}`);
+            const response = await trackerApi.post('signin/', { email, password});
+            await storeData('token',response.data.token)
+            dispatch({ type: actionTypes.SIGN_IN, payload: response.data.token });
+            navigate('TrackList');
+        } catch (error) {
+            dispatch({type: actionTypes.SIGNIN_ERROR, payload: getErrors(error.response.data) });
+        }
+};
+
 
 const signOut = (dispatch:()=>actionTypes.SignUpAction) => {
     return () => {
