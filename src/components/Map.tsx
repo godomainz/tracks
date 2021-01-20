8e7893152482231d746d58f8ca569a9c3bb03936
Akila Mendis <akila@oneit.com.au>
2021-01-20 12:52:59 +0530

    229 Showing a Map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 const Map = () => {
 
     return (
-        <View>
-            <Text>Hello from Map</Text>
-        </View>
+        <MapView initialRegion={{latitude: 37.33233 , longitude: -122.03121 , latitudeDelta: 0.01 ,longitudeDelta: 0.01}} style={styles.map} />
     );
 
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    map: {
+        height: 300
+    }
+});
 
 export default Map;
