6fff9a0b8f2e9e0aca040fe8274bf120157633a5
Akila Mendis <akila@oneit.com.au>
2021-01-22 15:33:11 +0530

    255 Avoiding Stale References

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -3,34 +3,38 @@ import { Accuracy, requestPermissionsAsync, watchPositionAsync } from 'expo-loca
 
 export default (shouldTrack: boolean, callback:(location:any)=>void) => {
     const [err, setErr] = useState<string | null>(null)
-    const [subscriber, setSubscriber] = useState<any>(null);
-
-    const startWatching = async () => {
-        try {
-            const { granted } = await requestPermissionsAsync();
-            if (!granted) {
-              throw new Error('Location permission not granted');
-            }else{
-                const sub = await watchPositionAsync({
-                    accuracy: Accuracy.BestForNavigation,
-                    timeInterval: 1000,
-                    distanceInterval: 1
-                }, (location)=>{
-                    callback(location);
-                });
-                setSubscriber(sub);
-            }
-          } catch (e) {
-            setErr(e);
-          }
-    }
 
     useEffect(() => {
+
+        let subscriber:any;
+
+        const startWatching = async () => {
+            try {
+                const { granted } = await requestPermissionsAsync();
+                if (!granted) {
+                  throw new Error('Location permission not granted');
+                }else{
+                    const subscriber = await watchPositionAsync({
+                        accuracy: Accuracy.BestForNavigation,
+                        timeInterval: 1000,
+                        distanceInterval: 1
+                    }, (location)=>{
+                        callback(location);
+                    });
+                }
+              } catch (e) {
+                setErr(e);
+              }
+        }
+
         if(shouldTrack){
             startWatching();
+            subscriber = null;
         }else{
-            subscriber.remove();
-            setSubscriber(null);
+            if(subscriber){
+                subscriber.remove();
+            }
+            subscriber = null;
         }
 
         return () => {
